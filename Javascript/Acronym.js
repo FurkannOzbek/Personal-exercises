30af78c168394f28e6dbe5aad82d018c7bf4cc5f
furkanozbek <furkannozbek@yandex.com>
2024-04-25 23:11:25 +0200

    First step of script added

---
+++ Javascript/Acronym.js
@@ -0,0 +1,8 @@
+// Help generate some jargon by writing a program that converts a long name
+// like Portable Network Graphics to its acronym (PNG).
+
+let givenWord = "Portable Network Graphics";
+let splitWords = givenWord.split(" ");
+//let a1 = givenWord.substring(0, givenWord.indexOf(" ") + 2);
+
+console.log(splitWords);
