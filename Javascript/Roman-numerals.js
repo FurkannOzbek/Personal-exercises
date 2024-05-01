a532df9a9b2d0e65d5ff4c7a252a574b0fa60212
furkanozbek <furkannozbek@yandex.com>
2024-05-02 01:45:54 +0200

    Long but working code for roman numbers

--- Javascript/Roman-numerals.js
+++ Javascript/Roman-numerals.js
@@ -1,73 +1,74 @@
-const number = 2795;
-const romen = [];
-const ones = (number % 100) % 10;
-const tens = Math.floor((number % 100) / 10);
-const hundreds = Math.floor((number % 1000) / 100);
-const thousands = Math.floor(number / 1000);
 // Rules for thousands
-function numbersToRoman(number) {
-  for (i = 0; i < thousands; i++) {
-    romen.push("M");
-  }
-  // Rules for hundreds
-  if (hundreds <= 3) {
-    for (i = 0; i < hundreds; i++) {
-      romen.push("C");
-    }
-  } else if (hundreds == 4) {
-    romen.push("CD");
-  } else if (hundreds == 5) {
-    romen.push("D");
-  } else if (hundreds > 5 && hundreds < 9) {
-    romen.push("D");
-    for (i = 5; i < hundreds; i++) {
-      romen.push("C");
-    }
-  } else if (hundreds == 9) {
-    romen.push("CM");
-  }
-  // Rules for tens
-  if (tens <= 3) {
-    for (i = 0; i < tens; i++) {
-      romen.push("X");
-    }
-  } else if (tens == 4) {
-    romen.push("IL");
-  } else if (tens == 5) {
-    romen.push("L");
-  } else if (tens > 5 && tens < 9) {
-    romen.push("L");
-    for (i = 5; i < tens; i++) {
-      romen.push("X");
-    }
-  } else if (tens == 9) {
-    romen.push("XC");
-  }
-  // Rules for ones
-  if (ones <= 3) {
-    for (i = 0; i < ones; i++) {
-      romen.push("I");
-    }
-  } else if (ones == 4) {
-    romen.push("IV");
-  } else if (ones == 5) {
-    romen.push("V");
-  } else if (ones > 5 && ones < 9) {
-    romen.push("V");
-    for (i = 5; i < ones; i++) {
-      romen.push("I");
-    }
-  } else if (ones == 9) {
-    romen.push("IX");
-  }
-}
+function numbersToRoman() {
+  let number = document.getElementById("givenNumber").value;
+  let sum = "";
+  const ones = (number % 100) % 10;
+  const tens = Math.floor((number % 100) / 10);
+  const hundreds = Math.floor((number % 1000) / 100);
+  const thousands = Math.floor(number / 1000);
 
-numbersToRoman();
-console.log(ones);
-console.log(tens);
-console.log(hundreds);
-console.log(thousands);
-console.log(romen);
+  if (number > 4000) {
+    sum =
+      "Please enter a number below 4000 , ancient rome had no numbers more than 4000";
+  } else {
+    for (i = 0; i < thousands; i++) {
+      sum = sum + "M";
+    }
+    // Rules for hundreds
+    if (hundreds <= 3) {
+      for (i = 0; i < hundreds; i++) {
+        sum = sum + "C";
+      }
+    } else if (hundreds == 4) {
+      sum = sum + "CD";
+    } else if (hundreds == 5) {
+      sum = sum + "D";
+    } else if (hundreds > 5 && hundreds < 9) {
+      sum = sum + "D";
+      for (i = 5; i < hundreds; i++) {
+        sum = sum + "C";
+      }
+    } else if (hundreds == 9) {
+      sum = sum + "CM";
+    }
+    // Rules for tens
+    if (tens <= 3) {
+      for (i = 0; i < tens; i++) {
+        sum = sum + "X";
+      }
+    } else if (tens == 4) {
+      sum = sum + "IL";
+    } else if (tens == 5) {
+      sum = sum + "L";
+    } else if (tens > 5 && tens < 9) {
+      sum = sum + "L";
+      for (i = 5; i < tens; i++) {
+        sum = sum + "X";
+      }
+    } else if (tens == 9) {
+      sum = sum + "XC";
+    }
+    // Rules for ones
+    if (ones <= 3) {
+      for (i = 0; i < ones; i++) {
+        sum = sum + "I";
+      }
+    } else if (ones == 4) {
+      sum = sum + "IV";
+    } else if (ones == 5) {
+      sum = sum + "V";
+    } else if (ones > 5 && ones < 9) {
+      sum = sum + "V";
+      for (i = 5; i < ones; i++) {
+        sum = sum + "I";
+      }
+    } else if (ones == 9) {
+      sum = sum + "IX";
+    }
+  }
+
+  document.getElementById("roman1").innerHTML = `Roman number : ${sum}`;
+}
 
 /* second solution
 function romanize(num) {
