6e47c765b791334573b5102e8c53d78229e25ed1
furkanozbek <furkannozbek@yandex.com>
2024-06-15 14:35:37 +0200

    update

--- Javascript/Class-Exercises/js3-week1/index.js
+++ Javascript/Class-Exercises/js3-week1/index.js
@@ -22,56 +22,59 @@ function autoRefresh() {
 setInterval("autoRefresh()", 2000);
 
 // LISTING ALL RACES
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetch("https://dog.ceo/api/breeds/list/all")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const elementDog = document.getElementById("users-list");
-//       let breeds = Object.keys(data.message);
-//       breeds.forEach((breed) => {
-//         const dogBreed = document.createElement("h1");
-//         dogBreed.innerHTML = breed;
-//         elementDog.appendChild(dogBreed);
-//       });
-//     })
-//     .catch((err) => {
-//       console.log("Oh noooo!!");
-//       console.log(err);
-//     });
-// });
 
-// // PICK RANDOM DOG PICTURE FROM THE LIST AND DISPLAY ITS NAME UNDER
+// Fetch the list of data
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((data) => {
+      // Same process as we did before about creating element and connecting it to the div
+      const elementDog = document.getElementById("users-list");
+      // Here I tried to use breeds.forEach() method but I couldnt use it. With some research I found out that in the fetched object dog breeds holds as keys so
+      // So I had to use Object.keys() method.
+      let breeds = Object.keys(data.message);
+      breeds.forEach((breed) => {
+        const dogBreed = document.createElement("h1");
+        dogBreed.innerHTML = breed;
+        elementDog.appendChild(dogBreed);
+      });
+    })
+    .catch((err) => {
+      console.log("Oh noooo!!");
+      console.log(err);
+    });
+});
 
-// // First of all I fetch the list from the API.
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetch("https://dog.ceo/api/breeds/list/all")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // Names were defined as object keys in the fetched data so to be able to use them I had to use Object.keys method.
-//       let breeds = Object.keys(data.message);
-//       // Picking a random breed
-//       const randomBreed = breeds[Math.floor(Math.random() * breeds.length)];
-//       return randomBreed;
-//     })
-//     .then((breed) => {
+// PICK RANDOM DOG PICTURE FROM THE LIST AND DISPLAY ITS NAME UNDER
 
-//       // For fetch the randomed breed I had to use template literals to be able to fetch data with variable
-//       return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-//         .then((response) => response.json())
-//         .then((dogData) => {
-
-//           // Creating the elements I will use for image and the title and connecting them with the div we will use.
-//           const elementDog = document.getElementById("users-list");
-//           const randomDogElement = document.createElement("img");
-//           const randomDogElementTitle = document.createElement("h1");
-//           randomDogElementTitle.textContent = breed;
-//           randomDogElement.src = dogData.message;
-//           elementDog.appendChild(randomDogElement);
-//           elementDog.appendChild(randomDogElementTitle);
-//         });
-//     })
-//     .catch((err) => {
-//       console.log("Oh noooo!!");
-//       console.log(err);
-//     });
-// });
+// First of all I fetch the list from the API.
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((data) => {
+      // Names were defined as object keys in the fetched data so to be able to use them I had to use Object.keys method.
+      let breeds = Object.keys(data.message);
+      // Picking a random breed
+      const randomBreed = breeds[Math.floor(Math.random() * breeds.length)];
+      return randomBreed;
+    })
+    .then((breed) => {
+      // For fetch the randomed breed I had to use template literals to be able to fetch data with variable
+      return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then((response) => response.json())
+        .then((dogData) => {
+          // Creating the elements I will use for image and the title and connecting them with the div we will use.
+          const elementDog = document.getElementById("users-list");
+          const randomDogElement = document.createElement("img");
+          const randomDogElementTitle = document.createElement("h1");
+          randomDogElementTitle.textContent = breed;
+          randomDogElement.src = dogData.message;
+          elementDog.appendChild(randomDogElement);
+          elementDog.appendChild(randomDogElementTitle);
+        });
+    })
+    .catch((err) => {
+      console.log("Oh noooo!!");
+      console.log(err);
+    });
+});
